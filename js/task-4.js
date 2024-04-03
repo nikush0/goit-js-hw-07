67ae1115402725663eb594d67dbcf26c8fc3f86c
nikush0 <bochkovskiynikita@gmail.com>
2024-04-03 20:35:26 +0300

    home work

---
+++ js/task-4.js
@@ -0,0 +1,25 @@
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (!email || !password) {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+
+  loginForm.reset();
+});
---
+++ js/task-6.js
@@ -0,0 +1,41 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const controls = document.getElementById('controls');
+const input = controls.querySelector('input');
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes() {
+  const amount = input.valueAsNumber;
+
+  if (amount < 1 || amount > 100 || isNaN(amount)) {
+    alert('Please enter a number between 1 and 100.');
+    return;
+  }
+
+  destroyBoxes();
+
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10;
+  }
+
+  input.value = '';
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
